e295185d60b5da83c398c50b1217f464087ac98a
MTOnline69 <97001154+MTOnline69@users.noreply.github.com>
2025-07-30 22:47:41 +0100

    some more menu stuff

--- scenes/ui/pause_menu.gd
+++ scenes/ui/pause_menu.gd
@@ -2,12 +2,15 @@ extends CanvasLayer
 
 var settings_menu_scene = preload("res://scenes/ui/settings.tscn")
 
+@onready var button_container = $Control/ButtonContainer
+
 func _on_resume_button_pressed() -> void:
 	Globals.toggle_pause_menu()
 
 func _on_settings_button_pressed() -> void:
 	var settings_instance = settings_menu_scene.instantiate()
 	add_child(settings_instance)
+	button_container.hide()
 
 func _on_menu_button_pressed() -> void:
 	Globals.toggle_pause_menu()

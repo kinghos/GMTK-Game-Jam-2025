06a2959b2d5653394c67ff3f332d70bf1d8979a9
MTOnline69 <97001154+MTOnline69@users.noreply.github.com>
2025-07-31 18:59:19 +0100

    test level button

--- scenes/ui/title_screen.gd
+++ scenes/ui/title_screen.gd
@@ -16,3 +16,6 @@ func _on_credits_button_pressed() -> void:
 
 func _on_exit_button_pressed() -> void:
 	get_tree().quit()
+
+func _on_test_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/levels/test_level.tscn")

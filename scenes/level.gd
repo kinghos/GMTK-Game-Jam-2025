a082f9d04dd811112c4bd8f827cc47ea2e0d3199
Kingshuk Hossain <kingshukhossain@outlook.com>
2025-07-31 10:52:12 +0100

    Updated GameUI to contain debug info

--- scenes/level.gd
+++ scenes/level.gd
@@ -7,3 +7,11 @@ func _ready():
 
 func _on_button_pressed() -> void:
 	game_ui.trigger_powerups_menu()
+
+func _process(delta: float) -> void:
+	$GameUI/PlayerSpeed.text = "Speed: " + str(Globals.player.speed)
+	$GameUI/StunTime.text = "StunTime: " + str(Globals.stun_time)
+	$GameUI/KickDistance.text = "KickDistance: " + str(Globals.kick_distance)
+	$GameUI/Lives.text = "Lives: " + str(Globals.lives)
+	$GameUI/LassoReach.text = "LassoReach: " + str(Globals.player_lasso_reach)
+	$GameUI/LassoSize.text = "LassoSize: " + str(Globals.lasso.MAX_LASSO_LENGTH)

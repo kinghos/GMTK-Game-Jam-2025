8e9008f7a5f7cfe65761e98410e214f1511d57c7
Kingshuk Hossain <kingshukhossain@outlook.com>
2025-07-31 19:07:17 +0100

    added stuns, pen icons

--- scenes/pen.gd
+++ scenes/pen.gd
@@ -3,8 +3,20 @@ class_name Pen
 
 var animals_in_auto_kick_area: Array[BaseAnimal]
 var animals_in_pen_enclosure: Array[BaseAnimal]
-@export var max_animals: int
 @onready var animals_count_label: Label = $AnimalsCount
+@onready var animal_icon: Sprite2D = $AnimalIcon
+
+@export var max_animals: int
+@export_enum("Sheep", "Cow", "Chicken") var animal_type: String
+
+const ANIMAL_ICONS = {
+	"Cow": preload("res://assets/graphics/sprites/cow_icon.png"),
+	"Sheep": preload("res://assets/graphics/sprites/cow_icon.png")
+}
+
+func _ready() -> void:
+	animal_icon.texture = ANIMAL_ICONS[animal_type]
+	
 
 func _process(delta: float) -> void:
 	animals_count_label.text = str(len(animals_in_pen_enclosure)) + " / " + str(max_animals)

514576ddaf4c556adde0d52344264d22f46402c4
Kingshuk Hossain <kingshukhossain@outlook.com>
2025-07-31 17:32:02 +0100

    Added font, updated pens to show max

--- scenes/pen.gd
+++ scenes/pen.gd
@@ -3,6 +3,11 @@ class_name Pen
 
 var animals_in_auto_kick_area: Array[BaseAnimal]
 var animals_in_pen_enclosure: Array[BaseAnimal]
+@export var max_animals: int
+@onready var animals_count_label: Label = $AnimalsCount
+
+func _process(delta: float) -> void:
+	animals_count_label.text = str(len(animals_in_pen_enclosure)) + " / " + str(max_animals)
 
 func _on_instant_kick_area_body_entered(body: Node2D) -> void:
 	if body is BaseAnimal:

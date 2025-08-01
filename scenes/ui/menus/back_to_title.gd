d70163c3b1cc1989ba6763dcb15c7559eb5a9451
MTOnline69 <97001154+MTOnline69@users.noreply.github.com>
2025-08-01 23:40:51 +0100

    fixed settings bit of the pause menu

--- scenes/ui/menus/back_to_title.gd
+++ scenes/ui/menus/back_to_title.gd
@@ -1,4 +1,12 @@
 extends Node
 
+func _ready():
+	process_mode = Node.PROCESS_MODE_ALWAYS
+
 func _on_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/ui/menus/title_screen.tscn")
+	if get_tree().is_paused():
+		# such a great way of doing this!
+		get_parent().get_parent().queue_free() # deleting the instantiated settings
+		get_parent().get_parent().get_parent().button_container.show() # show the pause menu buttons again
+	else:
+		get_tree().change_scene_to_file("res://scenes/ui/menus/title_screen.tscn")
--- scenes/ui/menus/settings.gd
+++ scenes/ui/menus/settings.gd
@@ -5,5 +5,8 @@ extends Control
 func _ready():
 	process_mode = Node.PROCESS_MODE_ALWAYS
 	if get_tree().is_paused():
-		$TextureRect.hide()
-		menu_back_arrow.hide()
+		$Background.queue_free()
+		var container = get_node("Container")
+		container.get_node("MenuBackArrow").size.x = 250
+		container.get_node("VolumeSlidersContainer").position = Vector2(710, 379)
+		container.get_node("ColorRect").position = Vector2(685, 365)

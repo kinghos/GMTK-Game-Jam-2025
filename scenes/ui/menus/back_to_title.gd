61ee5e95c4a5897492b48a38fca6c4610f0d8763
MTOnline69 <97001154+MTOnline69@users.noreply.github.com>
2025-08-02 00:13:50 +0100

    slightly fix my settings pause menu jank

--- scenes/ui/menus/back_to_title.gd
+++ scenes/ui/menus/back_to_title.gd
@@ -4,9 +4,9 @@ func _ready():
 	process_mode = Node.PROCESS_MODE_ALWAYS
 
 func _on_pressed() -> void:
-	if get_tree().is_paused():
-		# such a great way of doing this!
-		get_parent().get_parent().queue_free() # deleting the instantiated settings
-		get_parent().get_parent().get_parent().button_container.show() # show the pause menu buttons again
+	# such a great way of doing this!
+	if get_tree().is_paused() and get_parent().name == "Settings":
+		get_parent().hide()
+		get_parent().get_parent().get_node("ButtonContainer").show()
 	else:
 		get_tree().change_scene_to_file("res://scenes/ui/menus/title_screen.tscn")
--- scenes/ui/menus/pause_menu.gd
+++ scenes/ui/menus/pause_menu.gd
@@ -1,15 +1,13 @@
 extends CanvasLayer
 
-var settings_menu_scene = preload("res://scenes/ui/menus/settings.tscn")
-
 @onready var button_container = $Control/ButtonContainer
+@onready var settings: Control = $Control/Settings
 
 func _on_resume_button_pressed() -> void:
 	Globals.toggle_pause_menu()
 
 func _on_settings_button_pressed() -> void:
-	var settings_instance = settings_menu_scene.instantiate()
-	add_child(settings_instance)
+	settings.show()
 	button_container.hide()
 
 func _on_menu_button_pressed() -> void:

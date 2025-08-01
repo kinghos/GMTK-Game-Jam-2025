b459b1b269ff81279396b21d2a9e0b947ec5c67e
MTOnline69 <97001154+MTOnline69@users.noreply.github.com>
2025-08-01 13:34:06 +0100

    moved offscreen pointers out of the way of the HUD

--- scenes/animals/offscreen_pointer.gd
+++ scenes/animals/offscreen_pointer.gd
@@ -1,6 +1,7 @@
 extends Sprite2D
 
 const SCREEN_MARGIN: float = 30.0
+const SCREEN_MARGIN_TOP: float = 108.0
 
 func _ready():
 	hide()
@@ -22,7 +23,7 @@ func _process(_delta: float) -> void:
 	if not visible_rect.has_point(target_global_position) and !get_parent().in_pen:
 		show()
 		global_position.x = clamp(target_global_position.x, visible_rect.position.x + SCREEN_MARGIN, visible_rect.position.x + visible_rect.size.x - SCREEN_MARGIN)
-		global_position.y = clamp(target_global_position.y, visible_rect.position.y + SCREEN_MARGIN, visible_rect.position.y + visible_rect.size.y - SCREEN_MARGIN)
+		global_position.y = clamp(target_global_position.y, visible_rect.position.y + SCREEN_MARGIN_TOP, visible_rect.position.y + visible_rect.size.y - SCREEN_MARGIN)
 		rotation = (target_global_position - Globals.player.global_position).angle()
 	else:
 		hide()

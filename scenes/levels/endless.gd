84be9639b69533ff2d63fccc7d291fdfb7261dad
Kingshuk Hossain <kingshukhossain@outlook.com>
2025-08-02 01:01:01 +0100

    Endless changes

--- scenes/levels/endless.gd
+++ scenes/levels/endless.gd
@@ -5,5 +5,19 @@ var animal_numbers = {
 	"Cow": 12,
 	"Sheep": 12,
 }
+var upper_bounds = [[-497, 2418], [-224, 216]] # [xmin, xmax], [ymin, ymax]
+var lower_bounds = [[-497, 2418], [842, 1310]] # [xmin, xmax], [ymin, ymax]
 
-var scaling_mult
+var animal_scenes = {
+	"Chicken": preload("res://scenes/animals/chicken.tscn"),
+	"Cow": preload("res://scenes/animals/cow.tscn"),
+	"Sheep": preload("res://scenes/animals/sheep.tscn")
+}
+
+var scaling_mult = 1.5
+
+func generate_animals():
+	for animal: String in animal_numbers:
+		var new_animal: BaseAnimal = animal_scenes[animal].instantiate()
+		new_animal.global_position = Vector2(randi_range(upper_bounds[0][0], upper_bounds[0][1]), randi_range(upper_bounds[1][0], upper_bounds[1][1]))
+		

e012372b40b6ce8134ac46215b7c48eb50ed10e6
MTOnline69 <97001154+MTOnline69@users.noreply.github.com>
2025-08-02 11:22:14 +0100

    added ice (not used for now) + slightly pointer changes

--- scenes/animals/offscreen_pointer.gd
+++ scenes/animals/offscreen_pointer.gd
@@ -20,11 +20,11 @@ func _process(_delta: float) -> void:
 		screen_size
 	)
 	
-	if Globals.current_level.animals_on_screen.size() <= Globals.current_level.uncaptured_animal_number / 3:
-		if not visible_rect.has_point(target_global_position) and !get_parent().in_pen:
-			show()
-			global_position.x = clamp(target_global_position.x, visible_rect.position.x + SCREEN_MARGIN, visible_rect.position.x + visible_rect.size.x - SCREEN_MARGIN)
-			global_position.y = clamp(target_global_position.y, visible_rect.position.y + SCREEN_MARGIN_TOP, visible_rect.position.y + visible_rect.size.y - SCREEN_MARGIN)
-			rotation = (target_global_position - Globals.player.global_position).angle()
-			return
+	#if Globals.current_level.animals_on_screen.size() <= Globals.current_level.uncaptured_animal_number / 3:
+	if not visible_rect.has_point(target_global_position) and !get_parent().in_pen:
+		show()
+		global_position.x = clamp(target_global_position.x, visible_rect.position.x + SCREEN_MARGIN, visible_rect.position.x + visible_rect.size.x - SCREEN_MARGIN)
+		global_position.y = clamp(target_global_position.y, visible_rect.position.y + SCREEN_MARGIN_TOP, visible_rect.position.y + visible_rect.size.y - SCREEN_MARGIN)
+		rotation = (target_global_position - Globals.player.global_position).angle()
+		return
 	hide()
